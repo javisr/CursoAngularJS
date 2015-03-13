f15e72f0d42b114775e9593389b51c61b958a5c5
Javi Santos <javi@snowdropsolutions.co.uk>
2015-03-13 13:02:12 +0100

    guarda

---
+++ 12_Vistas/app.js
@@ -0,0 +1,26 @@
+angular.module('app', ['ngRoute'])
+  .config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+      when('/recetas', {
+        templateUrl: 'recetas.html',
+        controller: 'controlladorRecetas'
+      }).
+      when('/cocineros', {
+        templateUrl: 'cocineros.html',
+        controller: 'controlladorCocineros'
+      }).
+      otherwise({
+        redirectTo: '/principal'
+      });
+    }
+  ])
+  .controller('controlladorPrincipal', ['$scope', function($scope) {
+    $scope.mensaje = 'Estas en la página principal';
+  }])
+  .controller('controlladorRecetas', ['$scope', function($scope) {
+    $scope.mensaje = 'Estas en la sección de recetas';
+  }])
+  .controller('controlladorCocineros', ['$scope', function($scope) {
+    $scope.mensaje = 'Estas en la sección de cocineros';
+  }]);
