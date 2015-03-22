eea4d87af00b49f56d00b836a67ff93ef4726a7a
Javier <javier@MacBook-Pro-de-Javier.local>
2015-03-22 16:56:44 +0100

    /recetas dinamica

--- Proyecto/app/js/controllers/recetas-listado-ctrl.js
+++ Proyecto/app/js/controllers/recetas-listado-ctrl.js
@@ -1,9 +1,10 @@
 angular.module('recetarioApp')
 	.controller('recetasListadoCtrl', ['$scope', '$http',
 		function($scope, $http) {
+			$scope.recetas = [];
 			$http.get('http://javisr.net:3000/recetas')
 				.success(function(response) {
-					console.log(response);
+					$scope.recetas = response;
 				})
 				.error(function(data, status, headers, config) {
 					console.log('Algo fue mal pidiendo las recetas');
