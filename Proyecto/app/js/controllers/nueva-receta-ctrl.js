5c9af2c5a3848591a6c1d60778c74739c182b957
Javier <javier@MacBook-Pro-de-Javier.local>
2015-03-22 19:27:54 +0100

    añade recetas

--- Proyecto/app/js/controllers/nueva-receta-ctrl.js
+++ Proyecto/app/js/controllers/nueva-receta-ctrl.js
@@ -1,6 +1,38 @@
 angular.module('recetarioApp')
-	.controller('nuevaRecetaCtrl', ['$scope', '$http',
-		function($scope, $http) {
+	.controller('nuevaRecetaCtrl', ['$scope', '$http', '$window',
 
+		function($scope, $http, $window) {
+			$scope.receta = {};
+			$scope.receta.ingredientes = [];
+			$scope.ingrediente = {
+				nombre: '',
+				cantidad: ''
+			};
+			$scope.ingredientesInsertados = [];
+			$scope.insertarReceta = function() {
+				if ($scope.receta && $scope.receta.ingredientes.length > 0 && $scope.receta.nombre && $scope.receta.descripcion && $scope.receta.precio) {
+					$http.post('http://javisr.net:3000/recetas', $scope.receta)
+						.success(function(response) {
+							$window.location.href = '#/recetas/' + response.id;
+						})
+						.error(function(data, status, headers, config) {
+							console.log('No se insetaron los datos');
+						});
+				} else {
+					console.log('faltan datos');
+				}
+			};
+			$scope.insertarIngrediente = function() {
+				if ($scope.ingrediente.nombre && $scope.ingrediente.cantidad) {
+					var ingredienteTemporal = angular.copy($scope.ingrediente);
+					if ($scope.ingredientesInsertados.indexOf(ingredienteTemporal.nombre) < 0) {
+						$scope.ingredientesInsertados.push(ingredienteTemporal.nombre);
+						$scope.receta.ingredientes.push(ingredienteTemporal);
+					}
+				}
+			};
+			$scope.borrarIngrediente = function() {
+				console.log('borrarIngrediente');
+			};
 		}
 	]);
