79e9eeea484cd52fba8e3fc1e1faea6fa2751aea
Javier <javier@MacBook-Pro-de-Javier.local>
2015-03-22 18:20:28 +0100

    añade borrar recetas

--- Proyecto/app/js/controllers/receta-ctrl.js
+++ Proyecto/app/js/controllers/receta-ctrl.js
@@ -1,13 +1,23 @@
 angular.module('recetarioApp')
-	.controller('recetaCtrl', ['$scope', '$http', '$routeParams',
-		function($scope, $http, $routeParams) {
+	.controller('recetaCtrl', ['$scope', '$http', '$routeParams', '$window',
+
+		function($scope, $http, $routeParams, $window) {
 			$scope.receta = [];
 			$http.get('http://javisr.net:3000/recetas/' + $routeParams.id)
-				.success(function(response) {
+				.success(function(response, status) {
 					$scope.receta = response;
 				})
 				.error(function(data, status, headers, config) {
 					console.log('Algo fue mal pidiendo la receta ' + $routeParams.id);
 				});
+			$scope.borrarReceta = function() {
+				$http.delete('http://javisr.net:3000/recetas/' + $routeParams.id)
+					.success(function(response, status) {
+						$window.location.href = '/'
+					})
+					.error(function(data, status, headers, config) {
+						console.log('Algo fue mal borrando la receta ' + $routeParams.id);
+					});
+			};
 		}
 	]);
